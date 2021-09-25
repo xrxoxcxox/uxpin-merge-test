3cfa69456aeadfc8cc4ee27e83503a52108d516a
xrxoxcxox <watanuki@ju-zen.com>
2021-09-26 00:18:38 +0900

    Add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,3 @@
+import styles from '../styles/Button.module.css'
+
+export const Button = (): JSX.Element => <button type="button" className={styles.button}>test</button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { Button } from '../components/Button'
 
 const Home: NextPage = () => {
   return (
@@ -15,6 +16,7 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <Button />
       </main>
     </div>
   )
